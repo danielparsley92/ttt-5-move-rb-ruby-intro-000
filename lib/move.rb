4d35d50e00f3c3e74d9a47e225c5ab42f955b91c
Daniel Parsley <danielparsley92@gmail.com>
2021-02-16 17:29:35 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,5 +12,7 @@ end
 def update_array_at_with(array, index, value)
   array[index] = value
 end
-
+def board 
+   [" ", " ", " ", " ", " ", " ", " ", " "," "]
+ end
 update_array_at_with(board, 0, "X")
